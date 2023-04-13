f751f1974aea499901930552fc1c0145145e1bb6
Furkan Doğan <1furkandogan1@gmail.com>
2023-04-14 00:04:55 +0300

    added attribution

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import "./style.css";
 import { HiOutlineMoon } from "react-icons/hi";
+import { BsGithub } from "react-icons/bs";
 import { useDispatch, useSelector } from "react-redux";
 import { changeTheme, setTheme } from "../../redux/countrySlice";
 
@@ -24,6 +25,7 @@ export default function Navbar() {
     <nav>
       <div id="navContainer">
         <h1 id="logo">Where in the world?</h1>
+        <a href="https://www.github.com/doganfurkan" target="_blank" rel="noreferrer"><BsGithub/> Made by Furkan Doğan</a>
         <button id="themeChanger" onClick={() => {
           document.body.classList.toggle("dark");
           dispatch(changeTheme())
